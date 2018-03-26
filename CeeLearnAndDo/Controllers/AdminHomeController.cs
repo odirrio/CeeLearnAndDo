b76281fcd6403f9e875565bbfe3464cffb609964
peanutbutter03 <claudioferraro@live.nl>
2018-03-26 23:10:45 +0200

    added and started admin-template

---
+++ CeeLearnAndDo/Controllers/AdminHomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CeeLearnAndDo.Models;
+
+namespace CeeLearnAndDo.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
